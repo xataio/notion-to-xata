e9513089586178425009be893eee449d784fbca7
Tejas Kumar <tejas@tejas.qa>
2022-02-08 23:07:06 +0100

    Update domain

--- util/redirectUri.ts
+++ util/redirectUri.ts
@@ -1,4 +1,4 @@
 export const redirectUri =
   (process.env.NODE_ENV === "production"
-    ? "https://notion-xata.vercel.app"
+    ? "https://notion-to-xata.vercel.app"
     : "http://localhost:3000") + "/api/oauth/redirect";
