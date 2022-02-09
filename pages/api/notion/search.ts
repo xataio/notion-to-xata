7a21bdc447aca7540c8b22e55c2b5c6b2419e882
Tejas Kumar <tejas@tejas.qa>
2022-02-09 01:50:20 +0100

    Handle no title case

--- pages/api/notion/search.ts
+++ pages/api/notion/search.ts
@@ -20,16 +20,20 @@ const handler: NextApiHandler = async (req, res) => {
     },
   }).then((r) => r.json());
 
+  console.log(JSON.stringify(response, null, 2));
+
   if (response.code === "unauthorized") {
     res.setHeader("Set-Cookie", getNotionCookie("", new Date()));
     res.status(401).end(JSON.stringify({ error: "unauthorized" }));
     return;
   }
 
-  const dbs = response.results.map((r) => ({
-    id: r.id,
-    name: getValueByType(r.title[0]),
-  }));
+  const dbs = response.results.map((r) => {
+    return {
+      id: r.id,
+      name: r.title[0] ? getValueByType(r.title[0]) : "No Title",
+    };
+  });
 
   res.end(JSON.stringify({ dbs }));
 };
